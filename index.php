a4d4682123398a3366cdbfb3218af90dfdf17ae0
vRajnish <vsrs2023@gmail.com>
2018-05-31 07:51:40 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,66 @@
+<html>
+    <head>
+        <title>My Auto Help</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+    <nav class="navbar navbar-inverse ">
+        <div class="container-fluid">
+            <div class="navbar-header aclass">
+                <a class="navbar-brand" href="#">MY AUTO HELP</a>
+            </div>
+        </div>
+    </nav>
+    <div class="col-md-4 col-md-offset-4 main_div">
+        <form action="">
+            <div class="text-center">
+                <h2><b>LOG IN</b></h2>
+            </div>
+            <div class="text-center">
+                <i class="fa fa-user-circle fa-5x"></i>
+            </div>
+
+            <div class="form-group">
+                <label for="email">Email address:</label>
+                <input type="email" class="form-control" id="email" placeholder="Email">
+            </div>
+            <div class="form-group">
+                <label for="pwd">Password:</label>
+                <input type="password" class="form-control" id="pwd" placeholder="Password">
+            </div>
+            <div class="form-group">
+                <label><input type="checkbox"> Remember me</label>
+            </div>
+           <div class="form-group text-center">
+               <button type="submit" class="btn  btn-danger btn-block">Submit</button>
+           </div>
+            <div class="form-group text-center">
+            <h4><b>OR</b></h4>
+            </div>
+            <div class="form-group">
+                <a href="signup.html" class="btn btn-success btn-block">Create Account</a>
+            </div>
+
+        </form>
+    </div>
+    <div class="navbar_bottom">
+        <a href="#home" >Home</a>
+        <a href="#news">News</a>
+        <a href="#contact">Contact</a>
+    </div>
+    </body>
+</html>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Rajnish
+ * Date: 5/26/2018
+ * Time: 1:23 PM
+ */
